8eb9895faf89e4d8eeef7f157d6700a025b93c0e
Arfanul Shouid <arfanulshouid3999@gmail.com>
2024-04-19 15:13:49 +0100

    changed menu layout

--- main.cpp
+++ main.cpp
@@ -1,30 +1,53 @@
-#include "channel.h"
 #include <iostream>
+#include <fstream>
+#include "channel.h"
+
+using namespace std;
+
+// User enters their choice in the menu
+int choice;
+
+void menu()
+{
+    cout << "|-------------------------|\n";
+    cout << "| ****     Menu      **** |\n";
+    cout << "|-------------------------|\n";
+    // options of actions
+    cout << "| 1. See channel list     |\n";
+    cout << "| 2. Switch to channel    |\n";
+    cout << "| 3. See favorites        |\n";
+    cout << "| 0. Exit the program     |\n";
+    cout << "|-------------------------|\n";
+    cout << "Enter your choice > ";
+}
 
 int main() {
-    std::cout << "Welcome to Cloud TV\n";
+    cout << "Welcome to Cloud TV\n";
 
     ChannelManager channelManager;
 
-    int choice;
     do {
-        std::cout << "Menu:\n";
-        std::cout << "1. See channel list\n";
-        std::cout << "2. Exit the program\n";
-        std::cout << "Enter your choice: ";
-        std::cin >> choice;
+        menu();  // Display the menu
+        cin >> choice;
+        cin.ignore();  // Clear out any extra input
 
         switch (choice) {
         case 1:
             channelManager.displayChannels();
             break;
         case 2:
-            std::cout << "Exiting the program.\n";
+            cout << "Functionality to switch to a channel will be implemented here.\n";
+            break;
+        case 3:
+            cout << "Functionality to see favorites will be implemented here.\n";
+            break;
+        case 0:
+            cout << "Exiting the program.\n";
             break;
         default:
-            std::cout << "Invalid choice. Please try again.\n";
+            cout << "Invalid choice. Please try again.\n";
         }
-    } while (choice != 2);
+    } while (choice != 0);
 
     return 0;
 }
